e38d7d822728100501ab2024d4adfd8af186bb5e
Ashok Dey <ashokdey100@gmail.com>
2017-01-07 22:28:18 +0530

    added a simple node website

---
+++ first-web-app/README.md
@@ -0,0 +1,27 @@
+#Simple NodeJS Dynamic Website
+* This very simple web application.
+* This is the fifth app by me while learning Node
+* It uses **Express** and **Handlebars(hbs)** modules
+* It also uses **MaterializeCSS** 
+* It uses **Handlebars** as a templating engine
+* It also shows a 404 page in case of invalid urls
+* It logs the requests and timestamp in the **serve.log** file located in **logs** folder
+
+###How to Setup and Run?
+
+_In the terminal, type **npm install & npm start** to setup and run the application._
+_Open terminal, type **localhost:5000"**_
+
+###Screenshot
+![Main Page](screenshot-1.JPG)
+![404 Page](screenshot-2.JPG)
+
+###See maintaniance in action
+
+_Remove the comments in the line number 26-28 in _**serve.js**_ to see the maintainance state of the app_ 
+
+###Uses
+* You can use if for making your website
+* You can setup the routes in the **server.js** file according to your needs for the above purpose.
+
+**You liked it? _Star it_ !**
---
+++ simple-website/server.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+//creating a middleware
+app.use((req, res, next) => {
+    //get current time
+    let timestamp = new Date().toString();
+    let data = `${timestamp} ${req.method} ${req.url}\n`;
+    console.log(data);
+    fs.appendFile('./logs/server.log', data, (err) => {
+        if (err) {
+            console.log('Erroe logging data to file');
+        }
+    })
+    next();
+});
+
+// remove the comments for maintainance
+app.use((req, res, next) => {
+    res.render('maintainance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('toUpper', (text) => {
+    return text.toUpperCase();
+});
+
+//set the views template engine
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle : 'Welcome to my Page',
+        message : 'This is made using node',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle : 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        status : 400,
+        errorMessage : 'bad_request'
+    });
+});
+
+//in case of invalid urls
+app.get('*', (req, res) => {
+  res.render('404.hbs', {
+      pageTitle: '404 Not found'
+  });
+});
+
+app.listen(5000, () => {
+    console.log("Server Running at port number: 5000");
+});
