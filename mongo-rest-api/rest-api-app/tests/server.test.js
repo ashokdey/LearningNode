ae794f0ca2694790dd77b18b88f756ac313fbdfb
Ashok Dey <ashokdey100@gmail.com>
2017-01-19 14:48:11 +0530

    test case for GET /todos

--- mongo-rest-api/rest-api-app/tests/server.test.js
+++ mongo-rest-api/rest-api-app/tests/server.test.js
@@ -4,14 +4,40 @@ const {app}     = require('./../server/server');
 const {Todo}    = require('./../models/todo');
 const {User}    = require('./../models/user');
 
+
+// seed data 
+const dummyTodos = [{
+    text : 'first dummy todo'
+},{
+    text : 'second dummy todo'
+},{
+    text : 'third dummy todos'
+}];
+
+
 // make the DB empty first
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(dummyTodos)
+    }).then(() => done());
 });
 
 // write tests 
+describe ('GET /todos', () => {
+    it('should get all todos', (done) => {
+
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.data.length).toBe(3);
+        })
+        .end(done)
+    });
+});
+
 describe ('POST /todos', () => {
-    
+
     it('should create a new todo', (done) => {
         let text = 'make a test suit';
 
@@ -26,7 +52,7 @@ describe ('POST /todos', () => {
                 return done(err);
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -46,7 +72,7 @@ describe ('POST /todos', () => {
             }
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(3);
                 done();
             }).catch((err) => done(err));
         });
