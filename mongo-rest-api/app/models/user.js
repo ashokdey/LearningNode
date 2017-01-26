df5eeb597dd8d4eae96cd7bec505b75d04e894c3
Ashok Dey <ashokdey100@gmail.com>
2017-01-27 04:51:24 +0530

    hashed user password

--- mongo-rest-api/app/models/user.js
+++ mongo-rest-api/app/models/user.js
@@ -3,6 +3,7 @@ const mongoose  = require('mongoose');
 const validator = require('validator');
 const jwt       = require('jsonwebtoken');
 const _         = require('lodash');
+const bcrypt   = require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
     name : {
@@ -78,6 +79,25 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.access' : 'auth'
     })
 }
+
+UserSchema.pre('save', function(next) {
+    let user = this;
+
+    if (user.isModified('password')){
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) {
+
+            }
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        });
+    }
+    else {
+        next();
+    }
+});
 
 let User = mongoose.model('User', UserSchema);
 
