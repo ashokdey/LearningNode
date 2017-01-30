4331fc674b374e427fe304ff1a5edbc82498efe3
Ashok Dey <ashokdey100@gmail.com>
2017-01-30 10:43:05 +0530

    added test for DELETE /users/me/token

--- mongo-rest-api/app/tests/server.test.js
+++ mongo-rest-api/app/tests/server.test.js
@@ -308,3 +308,23 @@ describe('POST /users/login', () => {
         });
     });
 });
+
+describe('DELETE /users/me/token', () => {
+    it('should remove the valid token on logout', (done) => {
+
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth', dummyUsers[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+
+            User.findById(dummyUsers[0]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((err) => done(err));
+        });
+    });
+});
